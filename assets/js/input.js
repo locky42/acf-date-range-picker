3b4bf386011c6dfada58f9b2eacd4eb7deaf03ad
Max Zinchenko <zinchenko.m.42@gmail.com>
2022-06-09 10:37:12 +0300

    fix js

--- assets/js/input.js
+++ assets/js/input.js
@@ -2,16 +2,16 @@
 
 
 	/**
-	*  initialize_field
-	*
-	*  This function will initialize the $field.
-	*
-	*  @date	30/11/17
-	*  @since	5.6.5
-	*
-	*  @param	n/a
-	*  @return	n/a
-	*/
+	 *  initialize_field
+	 *
+	 *  This function will initialize the $field.
+	 *
+	 *  @date    30/11/17
+	 *  @since    5.6.5
+	 *
+	 *  @param    n/a
+	 *  @return    n/a
+	 */
 
 	function initialize_field($field) {
 
@@ -40,14 +40,18 @@
 		to.datepicker("option", "minDate", from.datepicker('getDate'));
 
 		$inputFrom.change(function () {
-			addDatesToHiddenField();
+			addDatesToHiddenField($field);
 		});
 
 		$inputTo.change(function () {
-			addDatesToHiddenField();
+			addDatesToHiddenField($field);
 		});
 
-		function addDatesToHiddenField() {
+		function addDatesToHiddenField($field) {
+			$inputFrom = $field.find('.wsr-date-range-from');
+			$inputTo = $field.find('.wsr-date-range-to');
+			$fieldToSave = $field.find('.field-to-save');
+
 			$from = $inputFrom.val();
 			$to = $inputTo.val();
 			if ((!$from) && (!$to)) {
@@ -62,15 +66,15 @@
 	if (typeof acf.add_action !== 'undefined') {
 
 		/*
-		*  ready & append (ACF5)
-		*
-		*  These two events are called when a field element is ready for initizliation.
-		*  - ready: on page load similar to $(document).ready()
-		*  - append: on new DOM elements appended via repeater field or other AJAX calls
-		*
-		*  @param	n/a
-		*  @return	n/a
-		*/
+        *  ready & append (ACF5)
+        *
+        *  These two events are called when a field element is ready for initizliation.
+        *  - ready: on page load similar to $(document).ready()
+        *  - append: on new DOM elements appended via repeater field or other AJAX calls
+        *
+        *  @param	n/a
+        *  @return	n/a
+        */
 
 		acf.add_action('ready_field/type=date_range_picker', initialize_field);
 		acf.add_action('append_field/type=date_range_picker', initialize_field);
@@ -79,14 +83,14 @@
 	} else {
 
 		/*
-		*  acf/setup_fields (ACF4)
-		*
-		*  These single event is called when a field element is ready for initizliation.
-		*
-		*  @param	event		an event object. This can be ignored
-		*  @param	element		An element which contains the new HTML
-		*  @return	n/a
-		*/
+        *  acf/setup_fields (ACF4)
+        *
+        *  These single event is called when a field element is ready for initizliation.
+        *
+        *  @param	event		an event object. This can be ignored
+        *  @param	element		An element which contains the new HTML
+        *  @return	n/a
+        */
 
 		$(document).on('acf/setup_fields', function (e, postbox) {
 
